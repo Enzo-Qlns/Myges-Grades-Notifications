d82f15a6e3c5ba63dce18b11961e14f3247645e5
Enzo QUELENIS <equelenis@myges.fr>
2024-10-02 20:28:31 +0200

    change method of init project

--- run.py
+++ run.py
@@ -1,30 +1,38 @@
 import time
 
-from services.cron_service import CronService
-from services.excel_service import ExcelService
-from services.mail_service import MailService
-from services.myges_service import MyGESService
-from services.logger_service import LoggerService
+from services.logger_service import FILE_NAME
 
-myges_service = MyGESService()
-excel_service = ExcelService(filename="grades.xlsx")
-logger = LoggerService('app.log')
-mail_service = MailService(logger=logger)
-cron_service = CronService(
-    myges_service=myges_service,
-    excel_service=excel_service,
-    mail_service=mail_service,
-)
 
-# Ajouter une tâche pour récupérer les notes toutes les heures (10 secondes)
-cron_service.add_job(cron_service.crontab_get_marks, interval_sec=5)
+def run():
+    from services.cron_service import CronService
+    from services.excel_service import ExcelService
+    from services.mail_service import MailService
+    from services.myges_service import MyGESService
+    from services.logger_service import LoggerService
 
-# Démarrer le service cron
-cron_service.start()
+    myges_service = MyGESService()
+    excel_service = ExcelService(filename="grades.xlsx")
+    logger = LoggerService(FILE_NAME)
+    mail_service = MailService(logger=logger)
+    cron_service = CronService(
+        myges_service=myges_service,
+        excel_service=excel_service,
+        mail_service=mail_service,
+    )
 
-# Garder le script en cours d'exécution
-try:
-    while True:
-        time.sleep(1)
-except KeyboardInterrupt:
-    cron_service.stop()
+    # Ajouter une tâche pour récupérer les notes toutes les 5 secondes
+    cron_service.add_job(cron_service.crontab_get_marks, interval_sec=5)
+
+    # Démarrer le service cron
+    cron_service.start()
+
+    # Garder le script en cours d'exécution
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        cron_service.stop()
+
+
+if __name__ == "__main__":
+    run()
