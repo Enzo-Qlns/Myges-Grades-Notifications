73b77d2fc35a368d94f546d6f5b8a17aeba9ef97
Enzo QUELENIS <equelenis@myges.fr>
2024-10-02 20:27:38 +0200

    change log message && add maximum number of lines

--- services/logger_service.py
+++ services/logger_service.py
@@ -1,5 +1,8 @@
 import logging
 
+FILE_NAME = 'app.log'
+MAX_LINES = 1000
+
 
 class LoggerService:
     def __init__(self, log_file):
@@ -7,7 +10,8 @@ class LoggerService:
         Initialize the LoggerService with a log file.
         :param log_file: The path to the log file.
         """
-        self.logger = logging.getLogger('my_logger')
+        self.log_file = log_file
+        self.logger = logging.getLogger()
         self.logger.setLevel(logging.DEBUG)
 
         # Create file handler which logs even debug messages
@@ -19,7 +23,7 @@ class LoggerService:
         ch.setLevel(logging.ERROR)
 
         # Create formatter and add it to the handlers
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         fh.setFormatter(formatter)
         ch.setFormatter(formatter)
 
@@ -27,17 +31,50 @@ class LoggerService:
         self.logger.addHandler(fh)
         self.logger.addHandler(ch)
 
-    def debug(self, message):
-        self.logger.debug(message)
+    def _truncate_log_file(self):
+        """
+        Truncate the log file if it exceeds the maximum number of lines.
+        """
+        with open(self.log_file, 'r') as file:
+            lines = file.readlines()
 
-    def info(self, message):
-        self.logger.info(message)
+        if len(lines) > MAX_LINES:
+            with open(self.log_file, 'w') as file:
+                file.writelines(lines[-MAX_LINES:])
 
-    def warning(self, message):
-        self.logger.warning(message)
+    def log(self, log_type: str, message: str):
+        """
+        Log a message with a specific type.
+        :param log_type: The type of the log message.
+        :param message: The message to log.
+        :return: None
+        """
+        log_methods = {
+            'debug': self.logger.debug,
+            'info': self.logger.info,
+            'warning': self.logger.warning,
+            'error': self.logger.error,
+            'critical': self.logger.critical
+        }
 
-    def error(self, message):
-        self.logger.error(message)
+        if log_type in log_methods:
+            log_methods[log_type](message)
+        else:
+            raise ValueError(f"Unknown log type: {log_type}")
 
-    def critical(self, message):
-        self.logger.critical(message)
+        self._truncate_log_file()
+
+    def debug(self, message: str):
+        self.log('debug', message)
+
+    def info(self, message: str):
+        self.log('info', message)
+
+    def warning(self, message: str):
+        self.log('warning', message)
+
+    def error(self, message: str):
+        self.log('error', message)
+
+    def critical(self, message: str):
+        self.log('critical', message)
