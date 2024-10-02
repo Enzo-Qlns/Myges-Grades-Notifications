59ae2f7987445a4e84da4674f0352e92717086c1
Enzo QUELENIS <equelenis@myges.fr>
2024-10-02 20:31:19 +0200

    add color echo

--- start_docker.sh
+++ start_docker.sh
@@ -13,13 +13,17 @@ if [ ! -f grades.xlsx ]; then
 fi
 
 # Pull the latest changes from the git repository
+echo -e "\e[1;32mPulling the latest changes from the git repository\e[0m"
 git pull
 
 # Remove the old services
+echo -e "\e[1;32mRemoving the old services\e[0m"
 docker compose down --rmi local
 
 # Build the new services
+echo -e "\e[1;32mBuilding the new services\e[0m"
 docker compose up --build -d
 
 # Show the running services
+echo -e "\e[1;32mShowing the running services\e[0m"
 tail -f app.log
