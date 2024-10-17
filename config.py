b808e94393543d1f6d1dd0c18729f96ef4f6727c
Enzo QUELENIS <equelenis@myges.fr>
2024-10-17 19:40:07 +0200

    remove useless env

--- config.py
+++ config.py
@@ -5,12 +5,6 @@ class Settings(BaseSettings):
     MYGES_LOGIN: str
     MYGES_PASSWORD: str
 
-    DESTINATION_EMAIL: str
-
-    MAILJET_API_KEY: str
-    MAILJET_SECRET_KEY: str
-    MAILJET_SENDER_EMAIL: str
-
     TELEGRAM_BOT_TOKEN: str
     TELEGRAM_CHANNEL_ID: str
 
